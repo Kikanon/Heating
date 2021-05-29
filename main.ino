92fef1d5811841c3da6c7c149aa96c4c65161d8b
Kikanon <48151998+Kikanon@users.noreply.github.com>
2021-05-29 14:03:07 +0200

    Add files via upload

--- main.ino
+++ main.ino
@@ -4,162 +4,143 @@
   #include <LiquidCrystal_I2C.h>
   #include <Wire.h>
   #include <OneWire.h>
+
+  #define pinOil 1
+  #define pinSensors 10 //pin po katerem komunicirajo senzorji za temparaturo
+  #define pinBoi 11
+  #define pinPec 12
+  #define debugMode 1
   
-  #define ONE_WIRE_BUS 10 //bus senzorji
-  float pec, boi;
-  bool stanje, stanjeb;
-  LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+  LiquidCrystal_I2C lcd(0x27, 16, 2);// lcd display 16*2 pikslov //0x27 je naslov kr so A0 A1 in A2 povezani
   //lcd A4 (SDA), A5 (SCL)
   /*-----( Declare objects )-----*/
   // Setup a oneWire instance to communicate with any OneWire devices
   // (not just Maxim/Dallas temperature ICs)
-  OneWire oneWire(ONE_WIRE_BUS);
-  int inPin = 1, olje = 1;
+  OneWire oneWire(pinSensors);
+  
   // Pass address of our oneWire instance to Dallas Temperature.
   DallasTemperature sensors(&oneWire);
-  
-  DeviceAddress Probe01 = { 0x28, 0xC2, 0x9B, 0x3E, 0x07, 0x00, 0x00, 0x0F }; // pec
-  DeviceAddress Probe02 = { 0x28, 0x77, 0x56, 0x3F, 0x07, 0x00, 0x00, 0xA2 }; //bojler
+
+  //naslovi da vsakic dobimo prave senzorje
+  //https://www.hacktronics.com/Tutorials/arduino-1-wire-address-finder.html
+  //rabim se tretjega
+  DeviceAddress addrPec = { 0x28, 0xC2, 0x9B, 0x3E, 0x07, 0x00, 0x00, 0x0F }; // pec
+  DeviceAddress addrBoi = { 0x28, 0x77, 0x56, 0x3F, 0x07, 0x00, 0x00, 0xA2 }; // bojler
+  DeviceAddress addrRet = { 0x28, 0x16, 0xEA, 0x69, 0x0A, 0x00, 0x00, 0xF9 }; // cev povratna topla voda
   
   
   void setup() {
-    Serial.begin(9600);
-    lcd.begin();
-    lcd.backlight();
-    //lcd.noBacklight() izklop
-    pinMode(12, OUTPUT);
-    digitalWrite(12, HIGH);
-    pinMode(11, OUTPUT);
-    digitalWrite(11, HIGH);
-    pinMode(inPin, INPUT_PULLUP);
+	if(debugMode){
+		Serial.begin(9600);
+	}
+    
     sensors.begin();
+    lcd.begin();
+    
+    lcd.backlight(); //lcd.noBacklight() izklop
+    pinMode(pinPec, OUTPUT);
+    digitalWrite(pinPec, HIGH);
+    pinMode(pinBoi, OUTPUT);
+    digitalWrite(pinBoi, HIGH);
+    pinMode(pinOil, INPUT_PULLUP);
+    
   
     // nastavimo resulucijo od 9 do 12 bit (nižnja resolucija hitrejša meritev)
-    sensors.setResolution(Probe01, 12);
-    sensors.setResolution(Probe02, 12);
+    sensors.setResolution(addrPec, 12);
+    sensors.setResolution(addrBoi, 12);
   
   }
-  
-  void loop()
-  {
-    merjenje_temperature();
-    krmilje();
-    krmiljeb();
-    ledlcd();
-    olje = digitalRead(inPin);
-  }
-  
-  /* podzanke*/
-  void merjenje_temperature()
-  {
-    sensors.requestTemperatures();
-  
-    lcd.setCursor(0, 0); //začne pisati  0 v linji 0
-    lcd.clear();
-    lcd.print("1:");
-    displayTemperature(Probe01);  //gre v nasledni podprogram
-  
-    lcd.setCursor(0, 1); //začne pisati 0 v linji 1
-    lcd.print("2:");
-    displayTemperature(Probe02);
-  
-    //delay(1500);
-  }
-  
-  void displayTemperature(DeviceAddress deviceAddress)
-  {
-    float tempC = sensors.getTempC(deviceAddress);
-  
-    if (tempC == -127.00) // Meritev nepravilna ali ni signala
-    {
-      lcd.print("Temperature Error");
-      Serial.println("error");
-    }
-    else
-    {
-      // Serial.print("temperatura: ");
-      // Serial.println(tempC);
-      lcd.print("C=");
-      lcd.print(tempC);
-      lcd.print("C");
-    }
-    if ((deviceAddress) == Probe01)
-    {
-      Serial.print("pec:");
-      pec = tempC;
-      Serial.print(pec);
-    }
-  
-    if ((deviceAddress) == Probe02)
-    {
-      Serial.print(" ,bojler:");
-      boi = tempC;
-      Serial.println(boi);
-    }
-  
-  }
-  
 
-  void krmilje()
-  {
-    lcd.setCursor(10, 0);
-    lcd.print("pec");
-    if (pec < 40)
-    {
-      stanje = 0;
-    }
-    if (pec >= 45)
-    {
-      stanje = 1;
-    }
-    stan();
+    float tempPec = 0, tempBoi = 0; //temparatura v peci in bojlerju
+    bool statePec = 0, stateBoi = 0, stateOil = 1; //stanje relejev a crpalke
+
+  void loop(){
+	sensors.requestTemperatures(); //dobi temparature
+	tempPec = getTemp(addrPec);
+	tempBoi = getTemp(addrBoi);
+	stateOil = !digitalRead(pinOil); // stanje olja
+	  
+    controlPec(); // logika
+    controlBoi();
+	
+	if(debugMode)debug(); //just for debug purposes
+    displayLCD();
   }
-  void krmiljeb()
-  {
-    lcd.setCursor(10, 1);
-    lcd.print("boi");
-    if ((((pec >= 50) && (boi < 70) && (pec - boi >= 5 )) || (pec >= 100)) || (olje == 0)){
-      //nea gre zadno cez 100 kr je spoder nastavleno da pr 73 izklop ali ce je vkloplen saltar za olno pec
-      stanjeb = 1;
-    }
-    else if ((((boi >= 73) && (pec < 85)) || (pec < 45) || (boi >= pec )) || (boi >= 73) || (olje == 1)){
-      //izklop pri temperaturah ali ce je temperatura in olje izklopljeno
-      stanjeb = 0;
-    }
-    stanb();
-    delay (1500);
-  }
-  void ledlcd()
-  {
-    if ((pec > 38) || (olje == 0))
-    {
-      lcd.backlight();
-    }
-    else if ((pec <= 35) && (olje == 1))
-    {
-      lcd.noBacklight();
-    }
   
+//short support functions
+float getTemp(DeviceAddress deviceAddress){
+	return sensors.getTempC(deviceAddress);
+}
+void setPec(bool state){
+    digitalWrite(pinPec, state);
+}	
+void setBoiler(bool state){
+    digitalWrite(pinBoi, state);
+}
+  
+void displayLCD(){
+    //spremenljivka lcd, velikost ekrana 16x2
+	if ((tempPec <= 35) && (stateOil == 1)){
+      lcd.noBacklight();
+	  return;
+    }
+    else lcd.backlight();
+
+    lcd.clear();
+    
+    lcd.setCursor(0, 0);// prva vrsta (pec)
+    lcd.print("Pec:");
+    lcd.print(tempPec);
+    lcd.print("C  ");
+    lcd.print(statePec==1?"ON":"OFF");
+
+    lcd.setCursor(0, 1);// druga vrsta (bojler)
+    lcd.print("Boi:");
+    lcd.print(tempBoi);
+    lcd.print("C  ");
+    lcd.print(stateBoi==1?"ON":"OFF");
+    }
+	
+void controlPec(){
+    if (tempPec < 40)
+    {
+      statePec = 0;
+    }
+    if (tempPec >= 45)
+    {
+      statePec = 1;
+    }
+	setPec(statePec);
+}
+void controlBoi(){
+    if (tempPec >= 50 && tempBoi < 70 && tempPec >= tempBoi + 5 || tempPec >= 100 || stateOil){
+      stateBoi = 1;
+    }
+    else if((tempPec < 45 || tempBoi >= tempPec  || tempBoi >= 73) && !stateOil){
+      stateBoi = 0;
+    }
+    setBoiler(stateBoi);
+	  delay(2000); //wait for temperature wiggle
   }
-  void stan()
-  {
-    if (stanje == 1 || (olje == 0)){
-      lcd.print("on");
-      digitalWrite(12, 0);
+  
+void debug(){
+	if (tempPec == -127.00 || tempBoi == -127.00) // Meritev nepravilna ali ni signala
+    {
+      Serial.println("Napaka na senzorju za temparaturo!!");
     }
-    else if (stanje == 0 && (olje == 1)){
-      lcd.print("off");
-      digitalWrite(12, 1);
-    }
-  }
-  void stanb()
-  {
-    if (stanjeb == 1){
-      lcd.print("on");
-      digitalWrite(11, 0);
-    }
-    else if (stanjeb == 0){
-      lcd.print("off");
-      digitalWrite(11, 1);
-    }
-  }
+	float tempPec = 0, tempBoi = 0; //temparatura v peci in bojlerju
+    bool statePec = 0, stateBoi = 0, stateOil = 1; //stanje relejev a crpalke
+	
+	Serial.println("Temparature(Pec Boi): ");
+	Serial.print(tempPec);
+	Serial.print(" ");
+	Serial.print(tempBoi);
+	
+	Serial.println("Stanja(Pec Boi Oil): ");
+	Serial.print(statePec);
+	Serial.print(" ");
+	Serial.print(stateBoi);
+	Serial.print(" ");
+	Serial.print(stateOil);
+}
